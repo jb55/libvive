7ffd7a043efb80757c4431d4148b023ffabec9db
William Casarin <bill@casarin.me>
2016-07-12 01:51:18 -0700

    notes, working parser

--- vivehid.c
+++ vivehid.c
@@ -5,6 +5,60 @@
 #include <hidapi/hidapi.h>
 
 typedef unsigned char u8;
+void print_report(u8 *buf, int len);
+
+enum packet_type {
+  PACKET_SIXAXIS         = 232,
+  PACKET_ANALOG          = 241,
+  PACKET_TRACKPAD        = 242,
+  PACKET_TRACKPAD_TAP    = 243,
+  PACKET_TRIGGER         = 244,
+  PACKET_TRIGGER_BUTTON  = 245,
+
+};
+
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)                    \
+  (byte & 0x80 ? '1' : '0'),                    \
+    (byte & 0x40 ? '1' : '0'),                  \
+    (byte & 0x20 ? '1' : '0'),                  \
+    (byte & 0x10 ? '1' : '0'),                  \
+    (byte & 0x08 ? '1' : '0'),                  \
+    (byte & 0x04 ? '1' : '0'),                  \
+    (byte & 0x02 ? '1' : '0'),                  \
+    (byte & 0x01 ? '1' : '0')
+
+void print_binary(u8 byte) {
+  fprintf(stderr, BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(byte));
+}
+
+void print_sixaxis(u8 *buf, int len) {
+  fprintf(stderr, "gyro %04hhd %04hhd\n", buf[9], buf[7]);
+}
+
+void print_buttons(u8 *buf) {
+  u8 typ = buf[4];
+  u8 buttons = buf[5];
+
+  /* print_binary(buttons); */
+  /* fprintf(stderr, "\n"); */
+  /* if (buttons & 0x40) */
+  /*   fprintf(stderr, "something\n"); */
+  if (buttons == 6)
+    fprintf(stderr, "trackpad_center\n", buttons);
+  if (buttons == 4)
+    fprintf(stderr, "trackpad_edge\n", buttons);
+  if (buttons == 32)
+    fprintf(stderr, "hamburger\n");
+  if (buttons == 16)
+    fprintf(stderr, "side_button\n");
+}
+
+void print_trackpad(u8 *buf, int len) {
+  char x = buf[6];
+  char y = buf[8];
+  fprintf(stderr, "pad %4hhd %4hhd\n", x, y);
+}
 
 int main(int argc, char* argv[])
 {
@@ -28,12 +82,12 @@ int main(int argc, char* argv[])
 	devs = hid_enumerate(0x0, 0x0);
 	cur_dev = devs;
 	while (1) {
-		printf("Device Found\n  type: %04hx %04hx\n  path: %s\n  serial_number: %ls",
+		fprintf(stderr, "Device Found\n  type: %04hx %04hx\n  path: %s\n  serial_number: %ls",
 			cur_dev->vendor_id, cur_dev->product_id, cur_dev->path, cur_dev->serial_number);
-		printf("\n");
-		printf("  Manufacturer: %ls\n", cur_dev->manufacturer_string);
-		printf("  Product:      %ls\n", cur_dev->product_string);
-		printf("\n");
+		fprintf(stderr, "\n");
+		fprintf(stderr, "  Manufacturer: %ls\n", cur_dev->manufacturer_string);
+		fprintf(stderr, "  Product:      %ls\n", cur_dev->product_string);
+		fprintf(stderr, "\n");
     if (cur_dev->next) {
       cur_dev = cur_dev->next;
     }
@@ -47,66 +101,94 @@ int main(int argc, char* argv[])
 	// Open the device using the VID, PID,
 	// and optionally the Serial number.
 	handle = hid_open(0x28de, 0x2101, L"CFAFE97AC4");
+	/* handle = hid_open(0x0bb4, 0x2c87, NULL); // vive */
 
   if (!handle) {
-    printf("Couldn't get hid handle %04hx %04hx\n",
+    fprintf(stderr, "Couldn't get hid handle %04hx %04hx\n",
               cur_dev->vendor_id, cur_dev->product_id);
     exit(1);
   }
 
 	// Read the Manufacturer String
 	res = hid_get_manufacturer_string(handle, wstr, MAX_STR);
-	printf("Manufacturer String: %ls\n", wstr);
+	fprintf(stderr, "Manufacturer String: %ls\n", wstr);
 
 	// Read the Product String
 	res = hid_get_product_string(handle, wstr, MAX_STR);
-	printf("Product String: %ls\n", wstr);
+	fprintf(stderr, "Product String: %ls\n", wstr);
 
 	// Read the Serial Number String
 	res = hid_get_serial_number_string(handle, wstr, MAX_STR);
-	printf("Serial Number String: %ls", wstr);
-	printf("\n");
+	fprintf(stderr, "Serial Number String: %ls", wstr);
+	fprintf(stderr, "\n");
 
 	// Send a Feature Report to the device
-	/* buf[0] = 0x1; // First byte is report number */
-	/* buf[1] = 0xa0; */
-	/* buf[2] = 0x0a; */
-	/* res = hid_send_feature_report(handle, buf, 17); */
-
-	// Set the hid_read() function to be non-blocking.
+	buf[0] = report; // First byte is report number
+	buf[1] = 0xff;
+  buf[2] = 0xff;
+  buf[3] = 0xff;
+  res = hid_send_feature_report(handle, buf, 17);
 
 	// Read a Feature Report from the device
   buf[0] = report;
-  printf("Feature Report %hhu\n", report);
   res = hid_get_feature_report(handle, buf, 64);
-  printf("Len: %d\n", res);
+  fprintf(stderr, "Feature Report %hhu (%d)\n", report, res);
 
   // Print out the returned buffer.
   for (i = 0; i < res; i++)
-    printf("%02hhx ", buf[i]);
-  printf("\n");
+    fprintf(stderr, "%02hhx ", buf[i]);
+  fprintf(stderr, "\n");
 
+  /* buf[1] = 0x81; */
+	/* hid_write(handle, buf, 1); */
 
-	hid_set_nonblocking(handle, 1);
+	hid_set_nonblocking(handle, 0);
 	// Send an Output report to toggle the LED (cmd 0x80)
 	/* buf[0] = 1; // First byte is report number */
 	/* buf[1] = 0x80; */
 	/* res = hid_write(handle, buf, 65); */
 
 	// Send an Output report to request the state (cmd 0x81)
-	/* buf[1] = 0x; */
-	/* hid_write(handle, buf, 65); */
+  buf[0] = report;
+  buf[1] = 0x81;
+  hid_write(handle, buf, 1);
 
 	// Read requested state
-  res = hid_read(handle, buf, 64);
   /* if (res < 0 || res < 78) */
-  printf("res: %d\n", res);
 
-  /* print_report(&buf[3], res - 3); */
-  for (i = 0; i < res; i++)
-    printf("%02hhx ", buf[i]);
-  printf("\n");
-
+  while (1) {
+    res = hid_read(handle, buf, 128);
+    if (!res) continue;
+    u8 buttons = buf[5];
+    switch (buf[4]) {
+    case PACKET_TRIGGER:
+      if (buttons >= 5)
+        fprintf(stderr, "trigger %hhu\n", buttons);
+      break;
+    case PACKET_TRIGGER_BUTTON:
+      if (buttons == 1)
+        fprintf(stderr, "trigger_click\n");
+      break;
+    case PACKET_ANALOG:
+      print_buttons(buf);
+      break;
+    case PACKET_TRACKPAD:
+      print_trackpad(buf, res);
+      break;
+    case PACKET_TRACKPAD_TAP:
+      if (buttons == 2)
+        fprintf(stderr, "trackpad_tap\n");
+      break;
+    case PACKET_SIXAXIS:
+      /* print_sixaxis(buf, res); */
+      break;
+    }
+    for (i = 0; i < res; i++)
+      /* printf("%02hhx ", buf[i]); */
+      printf("%02d:%03hhu ", i, buf[i]);
+      /* printf("%03hhu ", buf[i]); */
+    printf("\n");
+  }
 
 	return 0;
 }
